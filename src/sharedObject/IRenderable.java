d27f49b185540d6eca7986f160598edc6c680edd
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-04 00:57:20 +0700

    Create IRenderable.java

---
+++ src/sharedObject/IRenderable.java
@@ -0,0 +1,9 @@
+package sharedObject;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public interface IRenderable {
+	public int getZ();
+	public void draw(GraphicsContext gc);
+	public boolean isDestroyed();
+}
