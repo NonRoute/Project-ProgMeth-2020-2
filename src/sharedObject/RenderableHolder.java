b4f4d2df35b6f3d0d037658d5326d2e4a2898986
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-04 00:58:08 +0700

    Create RenderableHolder.java

---
+++ src/sharedObject/RenderableHolder.java
@@ -0,0 +1,11 @@
+package sharedObject;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class RenderableHolder {
+	
+	private List<IRenderable> entities;
+	private Comparator<IRenderable> comparator;
+
+}
