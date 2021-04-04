eb97ecbcd9417fb84e819be3368fa354b6c0efa5
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-04 17:24:23 +0700

    Update Entity.java

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,5 +3,18 @@ package entity;
 import sharedObject.IRenderable;
 
 public abstract class Entity implements IRenderable {
+	protected double x,y;
+	protected int z;
+	protected boolean visible;
+	
+	@Override
+	public boolean isVisible(){
+		return visible;
+	}
+	
+	@Override
+	public int getZ(){
+		return z;
+	}
 
 }
