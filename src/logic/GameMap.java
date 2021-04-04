edd781650626ee17fbf5b60bb1c697c4e49544cc
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-05 00:10:52 +0700

    Update GameMap.java

--- src/logic/GameMap.java
+++ src/logic/GameMap.java
@@ -1,5 +1,21 @@
 package logic;
 
-public class GameMap {
+import card.Card;
 
+public class GameMap {
+	private int numberOfRow = 5;
+	private int numberOfColumn = 9;
+	private Cell[][] cellMap;
+
+	public GameMap() {
+		cellMap = new Cell[numberOfRow][numberOfColumn];
+	}
+
+	public boolean setCardOnMap(Cell[][] cellMap, Card card, int row, int column) {
+		return cellMap[row][column].setCardOnCell(card);
+	}
+	
+	public boolean removeCardOnMap(Cell[][] cellMap, Card card, int row, int column) {
+		return cellMap[row][column].removeCardOnCell(card);
+	}
 }
