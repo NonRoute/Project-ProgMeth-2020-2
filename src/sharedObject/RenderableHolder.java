df4055cc00367b8ccd7c324fc3efbd504a5312f6
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-04 12:38:51 +0700

    Update RenderableHolder.java

--- src/sharedObject/RenderableHolder.java
+++ src/sharedObject/RenderableHolder.java
@@ -1,11 +1,43 @@
 package sharedObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class RenderableHolder {
-	
+	private static final RenderableHolder instance = new RenderableHolder();
+
 	private List<IRenderable> entities;
 	private Comparator<IRenderable> comparator;
+
+	public RenderableHolder() {
+		entities = new ArrayList<IRenderable>();
+		comparator = (IRenderable o1, IRenderable o2) -> {
+			if (o1.getZ() > o2.getZ())
+				return 1;
+			return -1;
+		};
+	}
+
+	public static RenderableHolder getInstance() {
+		return instance;
+	}
+
+	public void add(IRenderable entity) {
+		entities.add(entity);
+		Collections.sort(entities, comparator);
+	}
+
+	public void update() {
+		for (int i = entities.size() - 1; i >= 0; i--) {
+			if (entities.get(i).isDestroyed())
+				entities.remove(i);
+		}
+	}
+	
+	public List<IRenderable> getEntities() {
+		return entities;
+	}
 
 }
