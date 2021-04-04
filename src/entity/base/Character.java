f9b2c1c5dcbb89373b6d140935209e12c1621e8c
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-04 20:35:03 +0700

    Change class name

---
+++ src/entity/base/Character.java
@@ -0,0 +1,39 @@
+package entity.base;
+
+import java.util.ArrayList;
+
+import application.CSVParser;
+import card.Card;
+import card.base.FighterCard;
+import card.base.TrickCard;
+
+public abstract class Character extends Entity {
+
+	public static ArrayList<Card> importDeck(String filename){
+		ArrayList<Card> deck = new ArrayList<Card>();
+		String[][] deckData = CSVParser.readCSV(filename);
+
+		for (int i = 2; i < deckData.length; i++) { // each row = each card
+			System.out.println(deckData[i][0]);
+			if (deckData[i][0].equals("Fighter")) {
+				FighterCard card = new FighterCard();
+				card.setName(deckData[i][1]);
+				card.setAttackDamage(Integer.parseInt(deckData[i][2]));
+				card.setHeart(Integer.parseInt(deckData[i][3]));
+				card.setCost(Integer.parseInt(deckData[i][4]));
+				card.setSpeed(Integer.parseInt(deckData[i][5]));
+				card.setAttackRange(Integer.parseInt(deckData[i][6]));
+				card.setEffect(deckData[i][7]);
+				deck.add(card);
+
+			} else if (deckData[i][0].equals("Trick")) {
+				TrickCard card = new TrickCard();
+				card.setName(deckData[i][1]);
+				card.setCost(Integer.parseInt(deckData[i][4]));
+				card.setEffect(deckData[i][7]);
+				deck.add(card);
+			}
+		}
+		return deck;
+	}
+}
