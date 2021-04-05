6279978a6d447df8904e825ff3ac12467e09dbf0
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-05 14:19:39 +0700

    Update GameController.java

--- src/logic/GameController.java
+++ src/logic/GameController.java
@@ -10,13 +10,13 @@ import card.TrickCard;
 public class GameController {
 	private static ArrayList<Card> angelDeck;
 	private static ArrayList<Card> devilDeck;
-	private static ArrayList<Card> cardInPlayerHand;
-	private static ArrayList<Card> cardInBotHand;
+	private static ArrayList<Card> cardInLeftSideHand;
+	private static ArrayList<Card> cardInRightSideHand;
 	private static GameMap gameMap;
 	private static int turn;
-	private static int playerHeart;
-	private static int botHeart;
-	
+	private static Controller leftSideController;
+	private static Controller rightSideController;
+
 	static {
 		angelDeck = importDeck("AngelDeck.csv");
 		devilDeck = importDeck("DevilDeck.csv");
@@ -27,7 +27,7 @@ public class GameController {
 		String[][] deckData = CSVParser.readCSV(filename);
 
 		for (int i = 2; i < deckData.length; i++) { // each row = each card
-			
+
 			if (deckData[i][0].equals("Fighter")) {
 				FighterCard card = new FighterCard();
 				card.setName(deckData[i][1]);
@@ -49,36 +49,45 @@ public class GameController {
 		}
 		return deck;
 	}
-	
-	public static void startGame(Card playerCard, Card botCard, String gameMode) {
-		switch(gameMode) {
+
+	public static void startGame(Card playerCard, Card botCard, String botMode, String gameMode) {
+		switch (gameMode) {
+		case "PvB":
+			startGamePvB(playerCard, botCard, botMode);
+		case "PvP":
+			//WIP
+		case "BvB":	
+			//WIP
+		}
+	}
+
+	public static void startGamePvB(Card playerCard, Card botCard, String botMode) {
+		leftSideController = new Player();
+		switch (botMode) {
 		case "Easy":
-			playerHeart = 30;
-			botHeart = 20;
+			rightSideController = new BotEasy();
 		case "Normal":
-			playerHeart = 20;
-			botHeart = 20;
-		case "Hard" :
-			playerHeart = 20;
-			botHeart = 30;
+			rightSideController = new BotNormal();
+		case "Hard":
+			rightSideController = new BotHard();
 		}
 		gameMap = new GameMap();
 		turn = 0;
-		//cardInPlayerHand = random 4 card
-		//cardInBotHand = random 4 card
-		//while game not end
+		// cardInPlayerHand = random 4 card
+		// cardInBotHand = random 4 card
+		// while game not end
 		startTurn();
-		
+
 	}
+
 	public static void startTurn() {
 		turn++;
-		//TODO
+		// TODO
 	}
-	
+
 	public static Card drawCard(ArrayList<Card> deck) {
-		
+
 	}
-	
 
 	public static ArrayList<Card> getAngelDeck() {
 		return angelDeck;
