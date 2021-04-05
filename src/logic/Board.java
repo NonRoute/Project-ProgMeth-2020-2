15169f5ba0a89d3fb9af1f0c7ba3aa33ac0f5129
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-05 18:11:09 +0700

    Rename GameMap to Board

--- src/logic/Board.java
+++ src/logic/Board.java
@@ -2,13 +2,13 @@ package logic;
 
 import card.Card;
 
-public class GameMap {
-	private int numberOfRow = 5;
-	private int numberOfColumn = 9;
+public class Board {
+	private static final int NUMBER_OF_ROW = 5;
+	private static final int NUMBER_OF_COLUMN = 9;
 	private Cell[][] cellMap;
 
-	public GameMap() {
-		cellMap = new Cell[numberOfRow][numberOfColumn];
+	public Board() {
+		cellMap = new Cell[NUMBER_OF_ROW][NUMBER_OF_COLUMN];
 	}
 
 	public boolean setCardOnMap(Cell[][] cellMap, Card card, int row, int column) {
