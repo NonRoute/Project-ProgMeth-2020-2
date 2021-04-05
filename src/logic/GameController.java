aecfdc831ead12fb71e52600f27954a0ec476604
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-05 14:28:15 +0700

    Change ArrayList of Card to Deck

--- src/logic/GameController.java
+++ src/logic/GameController.java
@@ -6,10 +6,11 @@ import application.CSVParser;
 import card.Card;
 import card.FighterCard;
 import card.TrickCard;
+import deck.Deck;
 
 public class GameController {
-	private static ArrayList<Card> angelDeck;
-	private static ArrayList<Card> devilDeck;
+	private static Deck angelDeck;
+	private static Deck devilDeck;
 	private static ArrayList<Card> cardInLeftSideHand;
 	private static ArrayList<Card> cardInRightSideHand;
 	private static GameMap gameMap;
@@ -18,36 +19,8 @@ public class GameController {
 	private static Controller rightSideController;
 
 	static {
-		angelDeck = importDeck("AngelDeck.csv");
-		devilDeck = importDeck("DevilDeck.csv");
-	}
-
-	public static ArrayList<Card> importDeck(String filename) {
-		ArrayList<Card> deck = new ArrayList<Card>();
-		String[][] deckData = CSVParser.readCSV(filename);
-
-		for (int i = 2; i < deckData.length; i++) { // each row = each card
-
-			if (deckData[i][0].equals("Fighter")) {
-				FighterCard card = new FighterCard();
-				card.setName(deckData[i][1]);
-				card.setAttackDamage(Integer.parseInt(deckData[i][2]));
-				card.setHeart(Integer.parseInt(deckData[i][3]));
-				card.setCost(Integer.parseInt(deckData[i][4]));
-				card.setSpeed(Integer.parseInt(deckData[i][5]));
-				card.setAttackRange(Integer.parseInt(deckData[i][6]));
-				card.setEffect(deckData[i][7]);
-				deck.add(card);
-
-			} else if (deckData[i][0].equals("Trick")) {
-				TrickCard card = new TrickCard();
-				card.setName(deckData[i][1]);
-				card.setCost(Integer.parseInt(deckData[i][4]));
-				card.setEffect(deckData[i][7]);
-				deck.add(card);
-			}
-		}
-		return deck;
+		angelDeck = new Deck("Angel", "AngelDeck.csv");
+		devilDeck = new Deck("Devil", "DevilDeck.csv");
 	}
 
 	public static void startGame(Card playerCard, Card botCard, String botMode, String gameMode) {
@@ -55,9 +28,9 @@ public class GameController {
 		case "PvB":
 			startGamePvB(playerCard, botCard, botMode);
 		case "PvP":
-			//WIP
-		case "BvB":	
-			//WIP
+			// WIP
+		case "BvB":
+			// WIP
 		}
 	}
 
