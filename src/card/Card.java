76ed1066626ab42c76e8c7ce0cb5277568807449
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-07 23:13:39 +0700

    Update Card.java

--- src/card/Card.java
+++ src/card/Card.java
@@ -7,6 +7,7 @@ import logic.Direction;
 
 public abstract class Card extends Entity implements Cloneable {
 	private String name;
+	private String description;
 	private int cost;
 	private Effect effect;
 	private boolean isInHand;
@@ -30,6 +31,14 @@ public abstract class Card extends Entity implements Cloneable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public int getCost() {
