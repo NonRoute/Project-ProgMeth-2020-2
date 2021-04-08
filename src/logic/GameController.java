b900831720d8c6863c7ffc6fed356767ee5a4c4f
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-08 23:13:38 +0700

    Update GameController.java

--- src/logic/GameController.java
+++ src/logic/GameController.java
@@ -57,10 +57,28 @@ public class GameController {
 	public static void initializeGamePvP(Deck leftSideDeck, Deck rightSideDeck) {
 		leftSideController = new Player(20, 1, leftSideDeck, 4, Direction.LEFT);
 		rightSideController = new Player(20, 1, rightSideDeck, 4, Direction.RIGHT);
+		startGame();
 	}
 
-	startGame();
-
+	public static void initializeGameBvB(Deck leftSideDeck, Deck rightSideDeck, String leftDifficulty,
+			String rightDifficulty) {
+		switch (leftDifficulty) {
+		case "Easy":
+			leftSideController = new BotEasy(20, 1, rightSideDeck, 4, Direction.LEFT);
+		case "Normal":
+			leftSideController = new BotNormal(20, 1, rightSideDeck, 4, Direction.LEFT);
+		case "Hard":
+			leftSideController = new BotHard(30, 1, rightSideDeck, 4, Direction.LEFT);
+		}
+		switch (rightDifficulty) {
+		case "Easy":
+			rightSideController = new BotEasy(20, 1, rightSideDeck, 4, Direction.RIGHT);
+		case "Normal":
+			rightSideController = new BotNormal(20, 1, rightSideDeck, 4, Direction.RIGHT);
+		case "Hard":
+			rightSideController = new BotHard(30, 1, rightSideDeck, 4, Direction.RIGHT);
+		}
+		startGame();
 	}
 
 	public static void startGame() {
