98d322451b9c9557dc57e58b174d76c9d9e32e08
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-10 19:45:11 +0700

    Update Trickable.java

--- src/card/Trickable.java
+++ src/card/Trickable.java
@@ -1,10 +1,12 @@
 package card;
 
+import logic.Direction;
 import trick.Trick;
 
 public interface Trickable {
 
 	void activateTrick();
-	void setTrick(String trick, String trickParameter);
 	Trick getTrick();
+	void setTrick(Trick trick);
+	void setPlayingSide(Direction playingSide);
 }
