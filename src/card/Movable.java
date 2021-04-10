dcbbec53a3f14f5ccf6f0b151e851de8acd6e011
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-10 19:20:54 +0700

    Update Movable.java

--- src/card/Movable.java
+++ src/card/Movable.java
@@ -3,4 +3,15 @@ package card;
 public interface Movable {
 
 	void move();
+
+	public int getAttackDamage();
+	public void setAttackDamage(int attackDamage);
+	public int getAttackRange();
+	public void setAttackRange(int attackRange);
+	public int getHeart();
+	public void setHeart(int heart);
+	public int getSpeed();
+	public void setSpeed(int speed);
+	public int getCost();
+	public void setCost(int cost);
 }
