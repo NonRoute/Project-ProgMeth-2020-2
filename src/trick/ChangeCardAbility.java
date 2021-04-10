cc1a9c9c5c3425c16fee4f42ca91ca8fccb23d0c
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-10 18:22:07 +0700

    Update ChangeCardAbility.java

--- src/trick/ChangeCardAbility.java
+++ src/trick/ChangeCardAbility.java
@@ -1,5 +1,25 @@
 package trick;
 
-public class ChangeCardAbility extends Trick{
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class ChangeCardAbility extends Trick {
+	private char activateArea;
+	private char activateCondition;
+	private char conditionType;
+	private int conditionCost;
+	private int conditionAttackDamage;
+	private int conditionAttackRange;
+	private int conditionHeart;
+	private int conditionSpeed;
+	private int cost;
+	private int attackDamage;
+	private int attackRange;
+	private int heart;
+	private int speed;
+	
+	public ChangeCardAbility(String trickparameter) {
+		super(trickparameter);
+		activateArea = trickParameter.get(1);
+	}
 }
