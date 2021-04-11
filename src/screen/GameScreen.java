6f334e4bb9c40b06bd99072580affa7c235fbb12
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-04-11 21:52:17 +0700

    Update GameScreen.java

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -1,16 +1,43 @@
 package screen;
 
+import input.InputUtility;
+import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import logic.GameController;
+import logic.GameLogic;
 import sharedObject.IRenderable;
 import sharedObject.RenderableHolder;
 
-public class GameScreen extends Canvas {
+public class GameScreen {
+	private Pane root;
+	private Canvas canvas;
+	private GraphicsContext gc;
+	private GameLogic logic;
+
+	public GameScreen() {
+		root = new Pane();
+		canvas = new Canvas(1280, 720);
+		gc = canvas.getGraphicsContext2D();
+		logic = new GameLogic();
+		root.getChildren().addAll(canvas);
+		Scene scene = new Scene(root);
+		GameController.primaryStage.setScene(scene);
+		AnimationTimer animation = new AnimationTimer() {
+			public void handle(long now) {
+				paintComponent();
+				logic.logicUpdate();
+				RenderableHolder.getInstance().update();
+			}
+		};
+		animation.start();
+	}
 
 	public void paintComponent() {
-		GraphicsContext gc = this.getGraphicsContext2D();
-		gc.setFill(Color.BLACK);
+		gc.drawImage(RenderableHolder.backgroundGameScreen, 0, 0, 1280, 720);
 		for (IRenderable entity : RenderableHolder.getInstance().getEntities()) {
 			if (entity.isVisible()) {
 				entity.draw(gc);
